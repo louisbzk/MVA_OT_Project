b6ec4956f32438dbaf4d0102b3919d02139fbbf3
Louis <louisbf6@gmail.com>
2022-12-22 12:16:50 +0100

    [*] Setup for numerical experiments

---
+++ timer.py
@@ -0,0 +1,19 @@
+import time
+from typing import Callable
+
+
+def timeit(func: Callable, log_dict: dict):
+    """
+    A function decorator/wrapper that measures its execution time.
+
+    :param func: The function to run.
+    :param log_dict: A dictionary in which the time for each function is logged.
+    :return:
+    """
+    def wrapper(*args, **kwargs):
+        start = time.perf_counter()
+        result = func(*args, **kwargs)
+        end = time.perf_counter()
+        log_dict[func.__name__].append(end - start)
+        return result
+    return wrapper
